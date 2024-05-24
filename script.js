b5bb82d9a847e6b970cb7e9b04fd4eed5821049f
Miguel <22322402+mpuche3@users.noreply.github.com>
2024-05-24 09:10:25 +0000

    message

---
+++ script.js
@@ -0,0 +1,176 @@
+// git add .
+// git commit -m "message"
+// git push
+// git status
+// clear
+
+function get_tracks(){
+    const urls = [
+        "./transcriptions/books/B002/B002C000.json",
+        "./transcriptions/books/B002/B002C001.json",
+        "./transcriptions/books/B002/B002C002.json",
+        "./transcriptions/books/B002/B002C003.json",
+        "./transcriptions/books/B002/B002C004.json",
+        "./transcriptions/books/B002/B002C005.json",
+        "./transcriptions/books/B002/B002C006.json",
+        "./transcriptions/books/B002/B002C007.json",
+        "./transcriptions/books/B002/B002C008.json",
+        "./transcriptions/books/B002/B002C009.json",
+        "./transcriptions/books/B002/B002C010.json",
+        "./transcriptions/books/B002/B002C011.json",
+        "./transcriptions/books/B002/B002C012.json",
+        "./transcriptions/books/B002/B002C013.json",
+        // "./transcriptions/books/B002/B002C014.json",
+        // "./transcriptions/books/B002/B002C015.json",
+        // "./transcriptions/books/B002/B002C016.json",
+        "./transcriptions/books/B002/B002C017.json",
+        "./transcriptions/books/B002/B002C018.json",
+        "./transcriptions/books/B002/B002C019.json",
+
+        "./transcriptions/books/B001/B001C000.json",
+        "./transcriptions/books/B001/B001C002.json",
+        "./transcriptions/books/B009/B009C007.json",
+        "./transcriptions/books/B009/B009C008.json",
+        "./transcriptions/books/B009/B009C009.json",
+        "./transcriptions/books/B009/B009C011.json",
+    ]
+    let tracks = []
+    for (const url of urls) {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, false);
+        xhr.send();
+        tracks = tracks.concat(JSON.parse(xhr.responseText));
+    }
+    return tracks
+}
+
+const FactoryAudio = function () {
+    const tracks = get_tracks()
+    let audio = document.createElement('audio'); 
+    let itracks = 0;
+    let iCurrentTimes = -1;
+    let timeoutId_play;
+    let timeoutId_pause;
+    let promisePlay;
+    let status = "PAUSED";
+    
+    function changeTextColor() {
+        const button = document.querySelector('.button1');
+        button.style.color = 'grey';
+        setTimeout(() => {
+          button.style.color = 'white';
+        }, 50);
+    }
+
+    function getStatus () {
+        return status
+    }
+
+    function update_title(iCurrentTimes) {
+        const i = iCurrentTimes
+        const str_i = String(i).padStart(3, '0')
+        const book_chapter = tracks[itracks]["audioFileFullPath"].split("/").slice(-1)[0].split("_")[0] + "S" + str_i
+        const text = tracks[itracks]["currentTimes"][i]["tran"]
+        const currentTime = tracks[itracks]["currentTimes"][i]["ms"]
+        const nextTime = tracks[itracks]["currentTimes"][i + 1]["ms"]
+        const duration = nextTime - currentTime
+        const line_2 = "" +  `${currentTime.toFixed(3).padStart(7, '0')} [${duration.toFixed(3).padStart(7, '0')}]`
+        const title = document.querySelector("body > div.container > button");
+        title.innerHTML = `${book_chapter} </br> ${line_2} </br> </br> ${text}`
+    }
+    
+    function play() {
+        const playbackRate = 0.8
+        const additionalTimeintheloop = 500
+        const audioFileFullPath = tracks[itracks]["audioFileFullPath"];
+        const currentTime = tracks[itracks]["currentTimes"][iCurrentTimes]["ms"];
+        const nextTime = tracks[itracks]["currentTimes"][iCurrentTimes + 1]["ms"];
+        const duration = nextTime - currentTime;
+        clearTimeout(timeoutId_pause);
+        clearTimeout(timeoutId_play);
+        update_title(iCurrentTimes);
+        // WTF: 
+        // If audio.scr is set after audio.currentTime, 
+        // then audio.currentTime will be set to zero.
+        audio.src = audioFileFullPath;
+        audio.currentTime = currentTime;
+        audio.playbackRate = playbackRate;
+        audio.pause();
+        promisePlay = audio.play();
+        status = "PLAYING";
+        timeoutId_pause = setTimeout(_ => {
+            promisePlay.then(_ => {
+                audio.pause();
+            });
+        }, duration * 1000 * (1 / playbackRate))
+        timeoutId_play = setTimeout(_ => {
+            // changeTextColor()
+            play();
+        }, duration * 1000 * (1 / playbackRate) + additionalTimeintheloop);
+    }
+
+    function pause_play() {
+        console.log("PAUSE_PLAY")
+        clearTimeout(timeoutId_pause);
+        clearTimeout(timeoutId_play);
+        if (status === "PLAYING") {
+            promisePlay.then(_ => {
+                audio.pause();
+                status = "PAUSED";
+            });
+        } else {
+            play();
+        }
+    }
+
+    function playNext() {
+        console.log("NEXT")
+        iCurrentTimes += 1;        
+        if (iCurrentTimes === tracks[itracks]["currentTimes"].length - 1) {
+            nextTrack()
+        };
+
+        play();
+    }
+    
+    function playPrevious() {
+        iCurrentTimes -= 1;
+        if (iCurrentTimes < 0) {iCurrentTimes = 0};
+        play();
+    }
+
+    function nextTrack() {
+        iCurrentTimes = 0;
+        itracks += 1;
+        if (itracks === tracks.length) {itracks = 0};
+        play();
+    }
+
+    function previousTrack() {
+        iCurrentTimes = 0;
+        itracks -= 1;
+        if (itracks < 0) {itracks = 0};
+        play();
+    }
+
+    document.querySelector(".button1").addEventListener("click", playNext)
+    document.querySelector(".button2").addEventListener("click", playPrevious)    
+    document.querySelector(".button3").addEventListener("click", previousTrack)
+    document.querySelector(".button4").addEventListener("click", nextTrack)
+    document.querySelector(".button5").addEventListener("click", pause_play)
+
+    document.onkeydown = function (event) {
+        const callback = {
+            "ArrowLeft"  : playPrevious,
+            "ArrowRight" : playNext,
+            "ArrowUp"    : previousTrack,
+            "ArrowDown"  : nextTrack,
+            "Enter"      : pause_play,
+        }
+        callback[event.key]()
+    }
+    
+    return {audio, playPrevious, playNext, pause_play, play, getStatus, nextTrack, previousTrack}
+}
+
+const mpa = FactoryAudio()
