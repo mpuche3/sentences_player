a2fc363b1bb7f7c40eb93bd6969badccbdebb109
Miguel <22322402+mpuche3@users.noreply.github.com>
2024-05-27 13:10:59 +0000

    message

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function get_tracks_from_text(url) {
 function get_tracks(){
     const urls = [
         "./transcriptions/books/B001/B001_C001_C004.json",
-        "./transcriptions/books/B002/B002_C001_C040.txt",
+        "./transcriptions/books/B002/B002_C001_C130.txt",
         "./transcriptions/books/B009/B009_C001_C009.json",
         "./transcriptions/books/B009/B009_C010_C020.txt",
     ]
@@ -240,12 +240,12 @@ const FactoryAudio = function () {
 
     document.querySelector("#text").parentElement.addEventListener("click", sentence_up)
     document.querySelector("#pause").addEventListener("click", pause_play)    
-    document.querySelector("#book_up").addEventListener("click", book_up)
-    document.querySelector("#book_down").addEventListener("click", book_down)
-    document.querySelector("#chapter_up").addEventListener("click", chapter_up)
-    document.querySelector("#chapter_down").addEventListener("click", chapter_down)
-    document.querySelector("#sentence_up").addEventListener("click", sentence_up)
-    document.querySelector("#sentence_down").addEventListener("click", sentence_down)
+    document.querySelector("#book_up").addEventListener("click", book_down)
+    document.querySelector("#book_down").addEventListener("click", book_up)
+    document.querySelector("#chapter_up").addEventListener("click", chapter_down)
+    document.querySelector("#chapter_down").addEventListener("click", chapter_up)
+    document.querySelector("#sentence_up").addEventListener("click", sentence_down)
+    document.querySelector("#sentence_down").addEventListener("click", sentence_up)
     
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {sentence_up();}
